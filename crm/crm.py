ad0e9f32a8fc056adeb2b6e4b15da2416c992775
Paweł Płodzień <pawel.plodzien@gmail.com>
2018-11-05 16:12:58 +0100

    func get_name_by_id_from_table

--- crm/crm.py
+++ crm/crm.py
@@ -159,5 +159,11 @@ def get_name_by_id_from_table(table, id):
     Returns:
         str: the name of the customer
     """
+    id_index = 1
+    name_index = 2
+    for item in table:
+        if id == item[id_index]:
+            return item[name_index]
+    return None
 
     # your code
